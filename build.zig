b0a4f6388ec3a318936be691495e8fbd58a335ac
Wojtek <ws420@ic.ac.uk>
2024-12-01 17:42:30 +0000

    solved day 1

--- build.zig
+++ build.zig
@@ -1,91 +1,69 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "advent-of-code-2024",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    const num_days = 1;
+    const day: ?usize = b.option(usize, "ay", "Select day");
+    const test_step = b.step("test", "Run unit tests");
+    const run_step = b.step("run", "Run code");
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    if (day) |n| {
+        var steps = buildDay(b, n, target, optimize);
+
+        test_step.dependOn(&steps.run_tests.step);
+        run_step.dependOn(&steps.run_exe.step);
+
+        if (b.args) |args| {
+            steps.run_exe.addArgs(args);
+        } else {
+            steps.run_exe.addFileArg(b.path(b.fmt("inputs/{}", .{n})));
+        }
+    } else {
+        for (1..(num_days + 1)) |n| {
+            var steps = buildDay(b, n, target, optimize);
+
+            test_step.dependOn(&steps.run_tests.step);
+            run_step.dependOn(&steps.run_exe.step);
+
+            steps.run_exe.addFileArg(b.path(b.fmt("inputs/{}", .{n})));
+        }
+    }
+}
+
+const DaySteps = struct {
+    run_exe: *std.Build.Step.Run,
+    run_tests: *std.Build.Step.Run,
+};
+
+fn buildDay(
+    b: *std.Build,
+    n: usize,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) DaySteps {
+    const main_source = b.path(b.fmt("src/day{}.zig", .{n}));
 
     const exe = b.addExecutable(.{
-        .name = "advent-of-code-2024",
-        .root_source_file = b.path("src/main.zig"),
+        .name = b.fmt("day{}", .{n}),
+        .root_source_file = main_source,
         .target = target,
         .optimize = optimize,
     });
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = main_source,
         .target = target,
         .optimize = optimize,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    return .{ .run_exe = run_cmd, .run_tests = run_unit_tests };
 }
---
+++ src/day1.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var args = std.process.args();
+    _ = args.skip();
+    const input_path = args.next().?;
+    const input_file = try std.fs.cwd().openFile(input_path, .{});
+    defer input_file.close();
+    const input = try input_file.readToEndAlloc(allocator, 1_000_000);
+
+    var left = try std.ArrayList(usize).initCapacity(allocator, 1000);
+    var right = try std.ArrayList(usize).initCapacity(allocator, 1000);
+
+    var splitter = std.mem.split(u8, input, "\n");
+
+    while (splitter.next()) |line| {
+        if (line.len == 0) break;
+        var line_split = std.mem.split(u8, line, "   ");
+        const left_int = line_split.next().?;
+        const right_int = line_split.next().?;
+        try left.append(try std.fmt.parseInt(usize, left_int, 10));
+        try right.append(try std.fmt.parseInt(usize, right_int, 10));
+    }
+
+    std.mem.sort(usize, left.items, {}, comptime std.sort.asc(usize));
+    std.mem.sort(usize, right.items, {}, comptime std.sort.asc(usize));
+
+    var distance: usize = 0;
+    for (left.items, right.items) |l, r| {
+        distance += @max(l, r) - @min(l, r);
+    }
+
+    var similarity: usize = 0;
+    for (left.items) |item| {
+        similarity += item * (std.mem.count(usize, right.items, &[_]usize{item}));
+    }
+
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Distance: {}\nSimilarity Score: {}\n", .{distance, similarity});
+
+    try bw.flush();
+}
